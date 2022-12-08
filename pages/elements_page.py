1a0a2ea975f9d3010cdc0af1737fa2625984114a
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2022-12-08 11:53:24 +0300

    fixed_go_to_elements update elements test added text checkboxes

---
+++ pages/elements_page.py
@@ -0,0 +1,66 @@
+import random
+import time
+
+from selenium.webdriver.common.by import By
+
+from generator.generator import generated_persone
+from locators.elements_page_locators import TextBoxPageLocators, CheckBoxPageLocators
+from pages.base_page import BasePage
+
+
+class TextBoxPage(BasePage):
+    locators = TextBoxPageLocators()
+
+    def fill_all_fields(self):
+        person_info = next(generated_persone())
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
+        self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address, permanent_address
+
+
+    def check_filled_form(self):
+        full_name = self.element_is_present(self.locators.CREATED_FULL_NAME).text.split(':')[1]
+        email = self.element_is_present(self.locators.CREATED_EMAIL).text.split(':')[1]
+        current_address = self.element_is_present(self.locators.CREATED_CURRENT_ADDRESS).text.split(':')[1]
+        permanent_address = self.element_is_present(self.locators.CREATED_PERMANENT_ADDRESS).text.split(':')[1]
+        return full_name, email, current_address, permanent_address
+
+class CheckBoxPage(BasePage):
+    locators = CheckBoxPageLocators()
+    def open_full_list(self):
+        self.element_is_visible(self.locators.EXPAND_ALL_BUTTON).click()
+
+    def click_random_checkbox(self):
+        item_list = self.elements_are_visible(self.locators.ITEM_LIST)
+        count = 21
+        while count != 0:
+            item = item_list[random.randint(1, 15)]
+            if count > 0:
+                self.go_to_element(item)
+                item.click()
+                print(item)
+                count -=1
+            else:
+                break
+
+    def get_checked_checkboxes(self):
+        checked_list = self.elements_are_present(self.locators.CHECKED_ITEMS)
+        data = []
+        for box in checked_list:
+            title_item = box.find_element(By.XPATH, self.locators.TITLE_ITEM)
+            data.append(title_item.text)
+        return str(data).replace(' ', '').replace('doc', '').replace('.', '').lower()
+
+    def get_output_result(self):
+        result_list = self.elements_are_present(self.locators.OUTPUT_RESULT)
+        data = []
+        for item in result_list:
+            data.append(item.text)
+        return str(data).replace(' ', '').lower()
---
+++ tests/elements_test.py
@@ -0,0 +1,26 @@
+from pages.elements_page import TextBoxPage, CheckBoxPage
+import time
+
+
+class TestElements:
+    class TestTextBox:
+
+        def test_text_box(self, driver):
+            text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
+            assert full_name == output_name, "the full name does not match"
+            assert email == output_email, "the email does not match"
+            assert current_address == output_cur_addr, "the current address does not match"
+            assert permanent_address == output_per_addr, "the permanent address does not match"
+
+    class TestCheckBox:
+        def test_check_box(self, driver):
+            check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+            check_box_page.open()
+            check_box_page.open_full_list()
+            check_box_page.click_random_checkbox()
+            input_checkbox = check_box_page.get_checked_checkboxes()
+            output_result = check_box_page.get_output_result()
+            assert input_checkbox == output_result, 'checkboxes have not been selected'
