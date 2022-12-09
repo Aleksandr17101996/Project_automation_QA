307945b936ab08dc04d84b8b4587d92c5ef2062a
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2022-12-09 18:41:35 +0300

    add age, salary, department

--- data/data.py
+++ data/data.py
@@ -4,6 +4,11 @@ from dataclasses import dataclass
 @dataclass
 class Person:
     full_name: str = None
+    first_name: str = None
+    last_name: str = None
+    age: int = None
+    salary: int = None
+    department: str = None
     email: str = None
     current_address: str = None
     permanent_address: str = None
